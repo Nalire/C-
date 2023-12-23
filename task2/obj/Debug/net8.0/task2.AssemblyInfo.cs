4bca1022d2b393cea06511a6d977843e0a98f317
Arkadiy <arkadiy_2001@inbox.ru>
2023-12-23 14:21:32 +0600

    add task2

--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aafb96d9b3121ad33051304007b9b33701e7f1d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7546354b29e8f33b64a28fc52fe015a60852ae1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
